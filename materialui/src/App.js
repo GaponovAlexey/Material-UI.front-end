d02a7119dcda75cfef16b5f0fc302c15194b4f68
Алексей Гапонов <alexei.gaponov@gmail.com>
2021-07-12 16:43:49 +0300

    endTwo

--- materialui/src/App.js
+++ materialui/src/App.js
@@ -4,7 +4,7 @@ import Button from '@material-ui/core/Button';
 //import classes from './One.module.css';
 import {
   AppBar, Container, Toolbar,
-  IconButton, Typography, Box, Paper,
+  IconButton, Typography, Box, Paper, Grid,
 } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 import { makeStyles } from '@material-ui/core/styles';
@@ -18,6 +18,27 @@ const useStayles = makeStyles((theme) => ({
   },
   title: {
     flexGrow: 1
+  },
+  mainFeaturesPost: {
+    position: "relative",
+    color: theme.palette.common.white,
+    marginBottom: theme.spacing(4),
+
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center",
+  },
+  overlay: {
+    position: "absolute",
+    top: 0,
+    botton: 0,
+    right:0,
+    left: 0,
+    backgroundOverlay: "rgba(0,0,0,.3)"
+  },
+  mainFeaturesPostConteiner: {
+    position: "relative",
+    pading: theme.spacing(9)
   }
 }))
 
@@ -41,9 +62,39 @@ const One = () => {
         </Container>
       </AppBar>
       <main>
-        <Paper className={ classes.mainFeaturesPost }>
-          {/*Style={{backgroundImage: `url(https://source.unsplash.com/random)`}}*/ }
+        <Paper className={ classes.mainFeaturesPost }
+          style={ { backgroundImage: `url(https://source.unsplash.com/random)` } } >
+          <Container fixed>
+            <div className={ classes.overlay } />
+            <Grid container>
+              <Grid item md={ 6 }>
 
+                <div className={ classes.mainFeaturesPostConteiner }>
+                  <Typography
+                    component='h1'
+                    variant='h3'
+                    color='inherit'
+                    gutterBottom
+                  >
+                    Web My Man Post Blog
+                  </Typography>
+                  <Typography
+                    component='h5'
+                    variant='h5'
+                    color='inherit'
+                    paragraph
+                  >
+                    Web My About
+                    Ever needed custom formatted sample / test data, like, bad? Well, that's the idea of this script. It's a free, open source tool written in JavaScript, PHP and MySQL that lets you quickly generate large volumes of custom data in a variety of formats for use in testing software, populating databases, and... so on and so forth.
+
+                  </Typography>
+                  <Button variant="contained" color="secondary">
+
+                  </Button>
+                </div>
+              </Grid>
+            </Grid>
+          </Container>
         </Paper>
       </main>
 
